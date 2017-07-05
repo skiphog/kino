952f213be31da90313ef33ca0647846115255096
Skip <skiphog@mail.ru>
2017-07-05 21:30:57 +0300

    change factory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -1,5 +1,7 @@
 <?php
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Carbon\Carbon;
+
 $factory->define(App\Article::class, function (Faker\Generator $faker) {
     static $date;
 
@@ -7,7 +9,8 @@ $factory->define(App\Article::class, function (Faker\Generator $faker) {
         'title'       => $faker->words(3, true),
         'description' => $faker->words(5, true),
         'text'        => $faker->paragraphs(20, true),
-        'created_at'  => $date ?: $date = \Carbon\Carbon::now()->format('Y-m-d'),
+        'created_at'  => ($date instanceof Carbon) ?
+            $date = $date->modify('+15 days') : $date = Carbon::now()->modify('-15 months'),
         'updated_at'  => $date
     ];
 });
