4a540bc5e60735dbff0be11fc1073624c12285c8
Skip <skiphog@mail.ru>
2017-07-05 21:16:46 +0300

    add Factory, Article, templates, pagination

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -6,6 +6,10 @@ use App\Album;
 
 class AlbumController extends Controller
 {
+    /**
+     * Показать все альбомы со связанными песнями
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('pages.albums', [
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,6 +7,11 @@ use App\Article;
 class ArticleController extends Controller
 {
 
+    /**
+     * Показать все статьи
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \InvalidArgumentException
+     */
     public function index()
     {
         return view('pages.articles', [
@@ -14,6 +19,11 @@ class ArticleController extends Controller
         ]);
     }
 
+    /**
+     * Показать одну статью по id
+     * @param Article $article
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Article $article)
     {
         return view('pages.article', compact('article'));
