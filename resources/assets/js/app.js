a49c976e89a46d1ba8a6202800f827aba9f67744
Skip <skiphog@mail.ru>
2017-07-07 10:47:07 +0300

    add editor

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,9 +2,12 @@
 window.$ = window.jQuery = require('jquery')
 import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
+require('trumbowyg/dist/trumbowyg');
+require('trumbowyg/dist/langs/ru.min')
 
 // loads the Icon plugin
 UIkit.use(Icons)
+
 
 $(document).ready(function () {
 
@@ -16,5 +19,7 @@ $(document).ready(function () {
 
     });
   });
+
+  $('textarea').trumbowyg({lang: 'ru'});
 
 });
